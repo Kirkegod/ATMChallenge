98a74b9e1892c26f43563ba80b9cfe769391881e
pedro_bras <pedro.c.bras@gmail.com>
2019-06-11 13:55:17 +0200

    withdraw added and code refactored, all tests passing

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -8,17 +8,15 @@ describe Atm do
     subject.withdraw(50, account)
     expect(subject.funds).to eq 950
   end
-end
 
-describe Atm do
-  let(:account) { instance_double('Account') }
+  let (:account) { instance_double('Account') }
 
   before do
     allow(account).to receive(:balance).and_return(100)
     allow(account).to receive(:balance=)
   end
   it "allow withdraw if account has enough balance" do
-    expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+    expected_output = {status: true, message: 'success', date: Date.today, amount: 45}
     expect(subject.withdraw(45, account)).to eq expected_output
   end
 end
