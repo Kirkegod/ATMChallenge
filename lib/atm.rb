abcfb740c1c146412d38c016568281494de4240d
Kirkegaard <milla.kirkegaard@gmail.com>
2019-06-11 15:05:03 +0200

    reject transaction if atm is out of funds + test

--- lib/atm.rb
+++ lib/atm.rb
@@ -14,6 +14,8 @@ class Atm
         when insufficient_funds_in_account?(amount, account)
                 { status: false, message: 'insufficient funds', date: Date.today }
 
+        when insufficient_funds_in_atm?(amount)
+            { status: false, message: 'insufficient funds in ATM', date: Date.today }
         else
 
         perform_transaction(amount, account)
@@ -25,6 +27,11 @@ class Atm
 
     def insufficient_funds_in_account?(amount, account)
         amount > account.balance
+
+    end
+
+    def insufficient_funds_in_atm?(amount)
+        amount > @funds
     end
 
     def perform_transaction(amount, account)
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -25,7 +25,17 @@ describe Atm do
     expected_output = { status: false, message: 'insufficient funds', date: Date.today }
     expect(subject.withdraw(105, account)). to eq expected_output
   end
+  
+  it 'reject withdraw if ATM has insufficient funds' do
+    # To prepare the test we want to decrease the funds value
+    # to a lower value then the original 1000
+    subject.funds = 50
+    # Then we set the `expected_output`
+    expected_output = { status: false, message: 'insufficient funds in ATM', date: Date.today }
+    # And prepare our assertion/expectation
+    expect(subject.withdraw(100, account)).to eq expected_output
 
+  end
 # it 'gives status = true after successful withdrawal' do
 #     subject.withdraw(50)
 #     expect(subject.response()).to eq(true)
