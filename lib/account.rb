8b15529fa962b97c66467bd5cbf82d25a428de2e
Kirkegaard <milla.kirkegaard@gmail.com>
2019-06-12 15:33:09 +0200

    moved standard validity time outside initialize

--- lib/account.rb
+++ lib/account.rb
@@ -3,10 +3,12 @@ require "date"
 class Account
   attr_accessor :balance, :account_status, :exp_date, :pin_code
 
+  STANDARD_VALIDITY_YRS = 5 #why not instance or class variable? It crashes.
+
   def initialize
     @balance = 2000
     @account_status = :active
-    @exp_date = Date.today.next_year(5).strftime("%m/%y")
+    @exp_date = Date.today.next_year(STANDARD_VALIDITY_YRS).strftime("%m/%y")
     @pin_code = rand(1000..9999)
   end
 end
