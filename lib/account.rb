3027276dc2df91fd8f6c6dff30be5f036b83eeb4
pedro_bras <pedro.c.bras@gmail.com>
2019-06-12 10:03:47 +0200

    declared account class instance variables

--- lib/account.rb
+++ lib/account.rb
@@ -1,2 +1,9 @@
 class Account
+    attr_accessor :balance, :account_status, :exp_date, :pin_code
+
+    def initialize
+        @balance = 
+        @account_status =
+        @exp_date = 
+        @pin_code = rand(1000..9999)
 end
