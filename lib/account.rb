4e2fdbfdcf97568cd40abd754cad1b976b7361b9
Kirkegaard <milla.kirkegaard@gmail.com>
2019-06-12 15:25:16 +0200

    card is not expired works

--- lib/account.rb
+++ lib/account.rb
@@ -1,10 +1,12 @@
-class Account
-    attr_accessor :balance, :account_status, :exp_date, :pin_code
+require "date"
 
-    def initialize
-        @balance = 
-        @account_status =
-        @exp_date = 
-        @pin_code = rand(1000..9999)
-    end
+class Account
+  attr_accessor :balance, :account_status, :exp_date, :pin_code
+
+  def initialize
+    @balance = 2000
+    @account_status = :active
+    @exp_date = Date.today.next_year(5).strftime("%m/%y")
+    @pin_code = rand(1000..9999)
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,9 +1,15 @@
 require "./lib/account.rb"
 
 describe Account do
-    it 'check length of a number' do
-        number = 1234
-        number_length = Math.log10(number).to_i + 1
-        expect(number_length).to eq 4
-    end
+  it "check length of a number" do
+    number = 1234
+    number_length = Math.log10(number).to_i + 1
+    expect(number_length).to eq 4
+  end
+
+  it "is expected to have an expiry date on initialize" do
+    # Here we set the validity of the card to 5 yrs as default
+    expected_date = Date.today.next_year(5).strftime("%m/%y")
+    expect(subject.exp_date).to eq expected_date
+  end
 end
