8f832945649cf6ce0f5d1111c9171be1d895db03
Kirkegaard <milla.kirkegaard@gmail.com>
2019-06-13 12:35:07 +0200

    spec_file

--- lib/person.rb
+++ lib/person.rb
@@ -3,6 +3,7 @@ class Person
 
   def initialize(attrs = {})
     set_name(attrs[:name])
+    set_cash(attrs[:cash])
     @cash = 0
   end
 
@@ -10,7 +11,15 @@ class Person
     obj == nil ? missing_name : @name = obj
   end
 
+  def set_cash(obj)
+    obj == nil ? @cash = 0 : @cash = obj.to_i
+  end
+
   def missing_name
     raise "A name is required"
+  end
+
+  def self.create_account(attrs = {})
+    myAccount = self.class.new(attrs)
   end
 end
